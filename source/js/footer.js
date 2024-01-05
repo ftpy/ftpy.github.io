a06aa9dd7a593a7ff12da23d4a5762333a049190
罗小鱼 <90827451+sftpa@users.noreply.github.com>
2024-01-05 10:18:33 +0800

    2024

---
+++ source/js/footer.js
@@ -0,0 +1,10 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<center>\n' +
+    '<p style="color:#333;"><a href="https://cloud.tencent.com/login" target="_blank" style="color:#333;">©2024 XiaoYu</a></p>\n' +
+    '</center>\n' +
+    '');
+
+
+
+
---
+++ source/js/header.js
@@ -0,0 +1,11 @@
+document.write('<!-----------  公共js和css start----------->');
+document.write('' +
+    '<link rel="apple-touch-icon" sizes="76x76" href="source/img/favicon.ico">\n' +
+    '<link rel="shortcut icon" href="source/img/favicon.ico" type="image/x-png">\n' +
+    '<link rel="stylesheet" href="source/css/mystyle.css">\n' +
+    '');
+
+
+
+
+
