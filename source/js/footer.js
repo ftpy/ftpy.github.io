73a32acf7fe469252ec0e038e3769e5f187f21ef
小鱼开源 <90827451+ftpy@users.noreply.github.com>
2025-01-14 10:48:37 +0800

    Update footer.js

--- source/js/footer.js
+++ source/js/footer.js
@@ -1,7 +1,7 @@
 document.write('<!-----------  footer----------->');
 document.write('' +
     '<center>\n' +
-    '<p style="color:#333;"><a href="https://cloud.tencent.com/login" target="_blank" style="color:#333;">©2024 XiaoYu</a></p>\n' +
+    '<p style="color:#333;"><a href="https://cloud.tencent.com/login" target="_blank" style="color:#333;">©2025 XiaoYu</a></p>\n' +
     '</center>\n' +
     '');
 
